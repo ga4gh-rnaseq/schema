592506aef851f9c05d56896b24cf12cb87666a75
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-09-23 16:54:43 +0000

    Deploy ga4gh-rnaseq/schema to github.com/ga4gh-rnaseq/schema.git:gh-pages

--- ci/set-url-to-validate.sh
+++ ci/set-url-to-validate.sh
@@ -1,3 +1,6 @@
+export URL_START="https://raw.githubusercontent.com/ga4gh-rnaseq/schema/gh-pages/"
+export URL_END="openapi.yaml"
+
 export URL_BRANCH=$(
     if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
         if [ "$TRAVIS_BRANCH" = "master" ]; then
@@ -9,3 +12,5 @@ export URL_BRANCH=$(
         echo "preview/${TRAVIS_PULL_REQUEST_BRANCH}/";
     fi
 )
+
+export URL_TO_VALIDATE="${URL_START}${URL_BRANCH}${URL_END}"
